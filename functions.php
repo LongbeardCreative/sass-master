d7dbe3619047fc30bae08d8937514bcc52faf724
pwil30 <patrick@longbeardcreative.com>
2017-11-27 11:28:02 -0500

    fix to functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 Function wp_schools_enqueue_scripts() {
 wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
 wp_enqueue_style( 'childstyle' );
-wp_enqueue_script( 'lb-custom-js', get_stylesheet_directory_uri() . '/js/lb-custom.js', array( 'jquery' ) );
+wp_enqueue_script( 'lb_custom-js', get_stylesheet_directory_uri() . '/js/lb_custom.js', array( 'jquery' ) );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
 
